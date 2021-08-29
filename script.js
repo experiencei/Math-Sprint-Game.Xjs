18c14e2346682d49c99a77bd63bc5a1aec047b06
experiencei <ayelojahighbee01@gmail.com>
2021-08-29 09:39:28 +0100

    count down page working, awesome!

--- script.js
+++ script.js
@@ -83,6 +83,28 @@ function createEquations() {
 //   itemContainer.appendChild(bottomSpacer);
 // }
 
+// Displays 3, 2, 1, GO!
+function countdownStart() {
+  countdown.textContent = '3';
+  setTimeout(() => {
+    countdown.textContent = '2';
+  }, 1000);
+  setTimeout(() => {
+    countdown.textContent = '1';
+  }, 2000);
+  setTimeout(() => {
+    countdown.textContent = 'GO!';
+  }, 3000);
+}
+
+// Navigate from Splash Page to CountdownPage to Game Page
+function showCountdown() {
+  countdownPage.hidden = false;
+  splashPage.hidden = true;
+  countdownStart();
+  
+}
+
 // Get the value from selected radio button
 function getRadioValue() {
   let radioValue;
@@ -117,7 +139,6 @@ startForm.addEventListener('click', () => {
 });
 
 // Event Listeners
-gamePage.addEventListener('click', startTimer);
 startForm.addEventListener('submit', selectQuestionAmount);
 
 
